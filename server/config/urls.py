50566dbcb0d49c315e2f40a30c75cde6170225fe
KREZOO <krezoo.yt@gmail.com>
2025-05-16 19:00:09 +0300

    deployment setup #6

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 
 def index(request):
     return JsonResponse({"message": "Django backend is running."})
